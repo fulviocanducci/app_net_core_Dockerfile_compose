62df2b67337b9713731c1fba7d7ad9d1ee56b4ab
fulviocanducci <fulviocanducci@hotmail.com>
2021-04-24 11:35:05 -0300

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace app_net_docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
